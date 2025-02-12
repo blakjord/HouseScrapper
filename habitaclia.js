1deba1cbc5606d617a57b1eb2159bdb597a4804e
blaijor <blaijordansds@gmail.com>
2025-02-12 22:30:18 +0100

    script_v2

--- habitaclia.js
+++ habitaclia.js
@@ -4,7 +4,9 @@ const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
 
 async function habitaclia() {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
+   
+
     const page = await browser.newPage();
 
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36');
